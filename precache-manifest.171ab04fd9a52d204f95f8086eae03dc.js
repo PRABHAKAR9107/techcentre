7fc5889ea18caa119a088bd9a15c0d5f8c3d2016
PRABHAKAR9107 <02panjiyara@gmail.com>
2022-10-12 18:46:45 +0530

    Updates

---
+++ precache-manifest.171ab04fd9a52d204f95f8086eae03dc.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "476624d7d45aa341591e",
+    "url": "/techcentre/static/css/main.d0d02e63.chunk.css"
+  },
+  {
+    "revision": "476624d7d45aa341591e",
+    "url": "/techcentre/static/js/main.a3bb9728.chunk.js"
+  },
+  {
+    "revision": "fcdf28a9fd59d9bc3d5a",
+    "url": "/techcentre/static/js/runtime~main.ec5e4b43.js"
+  },
+  {
+    "revision": "394b49311e2deaf6133c",
+    "url": "/techcentre/static/css/2.6ec14f20.chunk.css"
+  },
+  {
+    "revision": "394b49311e2deaf6133c",
+    "url": "/techcentre/static/js/2.47896b8d.chunk.js"
+  },
+  {
+    "revision": "7709c10e8c30753537d94a6b69d7542a",
+    "url": "/techcentre/static/media/aboutBcg.7709c10e.jpeg"
+  },
+  {
+    "revision": "984874d19d1cdef8dcee5fcea55c082d",
+    "url": "/techcentre/static/media/mainBcg.984874d1.jpeg"
+  },
+  {
+    "revision": "160fddff3ca71887ebf8da3929244379",
+    "url": "/techcentre/static/media/productsBcg.160fddff.jpeg"
+  },
+  {
+    "revision": "945afb997e0731b038ed58fadacfeb3f",
+    "url": "/techcentre/static/media/contactBcg.945afb99.jpeg"
+  },
+  {
+    "revision": "9675841294261b773267d557f531a8ff",
+    "url": "/techcentre/static/media/singleProductBcg.96758412.jpeg"
+  },
+  {
+    "revision": "46143c2f25bc4667fac18f5924288217",
+    "url": "/techcentre/static/media/storeBcg.46143c2f.jpeg"
+  },
+  {
+    "revision": "0819a81634391f8518fe325378b8f994",
+    "url": "/techcentre/static/media/defaultBcg.0819a816.jpeg"
+  },
+  {
+    "revision": "2b362308d9a07ef7d73f4865cbd132ab",
+    "url": "/techcentre/static/media/logo.2b362308.svg"
+  },
+  {
+    "revision": "2f6cb1e15ae1d3b5e02f4d9e83509ff9",
+    "url": "/techcentre/index.html"
+  }
+];
